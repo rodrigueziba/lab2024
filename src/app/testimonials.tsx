e66eecc5e818d0dc23bde1504386a9341e3d3a8a
Ezequiel Ibarra <chimoibarra@gmail.com>
2024-02-22 21:13:00 -0300

    septima correcion

--- src/app/testimonials.tsx
+++ src/app/testimonials.tsx
@@ -38,17 +38,7 @@ export function Testimonials() {
           <div className="mb-10 flex h-12 w-12 items-center justify-center rounded-lg bg-gray-900 text-white ">
             <UserIcon className="h-6 w-6" />
           </div>
-          <Typography variant="h2" color="blue-gray" className="mb-2">
-            What Clients Say
-          </Typography>
-          <Typography
-            variant="lead"
-            className="mb-10 max-w-3xl text-center !text-gray-600"
-          >
-            Discover what our valued clients have to say about their experiences
-            with our services. We take pride in delivering exceptional results
-            and fostering lasting partnerships.
-          </Typography>
+          
         </div>
         <div className="grid grid-cols-1 gap-x-8 gap-y-12 md:grid-cols-3 lg:px-20">
           {TESTIMONIALS.map((props, key) => (
--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -18,18 +18,7 @@ export function TestimonialCard({
     <Card shadow={false} className="items-center text-center">
       <CardBody>
         <Avatar src={img} className="mb-3" alt={client} size="lg" />
-        <Typography variant="h6" color="blue-gray">
-          {client}
-        </Typography>
-        <Typography variant="small" className="mb-3 font-medium !text-gray-700">
-          {title}
-        </Typography>
-        <Typography
-          variant="paragraph"
-          className="mb-5 font-normal !text-gray-500"
-        >
-          &quot;{feedback}&quot;
-        </Typography>
+       
       </CardBody>
     </Card>
   );
